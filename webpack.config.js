2b2e1b1c8738b086d133df1ca5a5c916fb706d2a
Alexander <xspikyx@gmail.com>
2022-12-03 10:19:44 +0300

    -a.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@
 
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const TerserWebpackPlugin = require('terser-webpack-plugin');
+// const ImageminPlugin = require('imagemin-webpack');
 
 
 const isDev = process.env.NODE_ENV === 'development';
@@ -21,11 +21,11 @@ const optimization = () => {
     }
   };
 
-  if (isProd) {
-    configObj.minimizer = [
-      new TerserWebpackPlugin()
-    ];
-  }
+  // if (isProd) {
+  //   configObj.minimizer = [
+  //     new TerserWebpackPlugin()
+  //   ];
+  // }
 
   return configObj;
 };
@@ -40,10 +40,10 @@ const plugins = () => {
       }
     }),
 
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: `./css/${filename('css')}`
-    }),
+    // new CleanWebpackPlugin(),
+    // new MiniCssExtractPlugin({
+    //   filename: `./css/${filename('css')}`
+    // }),
     
   ];
 
@@ -62,8 +62,6 @@ module.exports = {
     publicPath: ''
   },
   devServer: {
-    historyApiFallback: true,
-    // contentBase: path.resolve(__dirname, 'dist'),
     open: true,
     compress: true,
     hot: true,
@@ -82,7 +80,7 @@ module.exports = {
         test: /\.css$/i,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            // loader: MiniCssExtractPlugin.loader,
             options: {
               hmr: isDev
             },
@@ -94,7 +92,7 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader,
+            // loader: MiniCssExtractPlugin.loader,
             options: {
               publicPath: (resourcePath, context) => {
                 return path.relative(path.dirname(resourcePath), context) + '/';
